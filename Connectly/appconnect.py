dee2970a3597f3768acf1f82312467429038abfb
LeoShadic345 <leonardoshadow345@gmail.com>
2024-11-14 15:40:14 -0500

    feed: added comments to appconnect

--- Connectly/appconnect.py
+++ Connectly/appconnect.py
@@ -1,17 +1,17 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-
+## Cargar el dataset
 df = pd.read_csv('data/SocialMediaUsersDataset.csv').head(1500) 
 
+## Conexión a la base de datos
 db_url = 'mysql+mysqlconnector://root:password@localhost:3306/connectly' 
 engine = create_engine(db_url)
 
-
+## Insertar el dataset en la base de datos
 columns_to_store = ['UserID', 'Name', 'Gender', 'DOB', 'Interests', 'City', 'Country']
 
 df[columns_to_store].to_sql('social_media_users', con=engine, if_exists='replace', index=False)
 
+## Mensaje de confirmación
 print("El dataset ha sido insertado correctamente en la base de datos MySQL.")
-
-## Sonic es god tier
